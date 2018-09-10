2129367196488cac7c04f06761639ed5093485d7
缐英达 <daegis@yeah.net>
2018-09-10 21:01:18 +0800

    delete useless code

--- src/test/java/com/hnair/spring/boot/mybatis/MybatisSpringBootStarterApplicationTests.java
+++ src/test/java/com/hnair/spring/boot/mybatis/MybatisSpringBootStarterApplicationTests.java
@@ -12,16 +12,4 @@ public class MybatisSpringBootStarterApplicationTests {
     @Test
     public void contextLoads() {
     }
-/**
- * initialSize                                   10
- * minIdle                                   10
- * maxActive                                   200
- * maxWait                                   60000
- * timeBetweenEvictionRunsMillis                                   60000
- * minEvictableIdleTimeMillis                                   300000
- * validationQuery                                   SELECT now()
- * testWhileIdle                                   true
- * testOnBorrow                                   false
- * testOnReturn                                   false
- */
 }
